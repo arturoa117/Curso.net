a335c05fb14222802a9b263b7a7bf7260815263b
arturoa117 <arturoapontebonilla117@gmail.com>
2024-08-03 17:17:05 -0400

    Practica 2

--- Practica 1/Practica 1-1/Practica 1-1/obj/Debug/net8.0/Practica 1-1.AssemblyInfo.cs
+++ Practica 1/Practica 1-1/Practica 1-1/obj/Debug/net8.0/Practica 1-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica 1-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3edfaeb7dad33e4077b1aae8636f562d9d79b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica 1-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica 1-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Practica 2/Practica 2-2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+try
+{
+    Console.WriteLine("Ingrese la primera calificación: ");
+    int cal1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la segunda calificación: ");
+    int cal2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la tercera calificación: ");
+    int cal3 = Convert.ToInt32(Console.ReadLine());
+
+
+    int promedio = (cal1 + cal2 + cal3) / 3;
+
+    if (promedio >= 7)
+    {
+        Console.WriteLine("Su promedio es de: " + {promedio} + " Usted ha aprobado!!!");
+    }
+    else
+    {
+        Console.WriteLine("Su promedio es de: " + {promedio} + " Usted se quemó");
+    }
+
+}
+catch (Exception e) {
+    
+    Console.WriteLine("Ha ocurrido un error " + {e.Message});
+}
